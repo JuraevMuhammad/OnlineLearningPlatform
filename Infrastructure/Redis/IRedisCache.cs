64c7879f45bec5942877b7769eb58976875e8fa3
juraev <muhammadjonjuraev18@gmail.com>
2026-02-21 17:14:37 +0500

    add resdis cache

--- Infrastructure/Redis/IRedisCache.cs
+++ Infrastructure/Redis/IRedisCache.cs
@@ -2,5 +2,7 @@
 
 public interface IRedisCache
 {
-    
+    Task<T?> GetDataAsync<T>(string key, CancellationToken cancellationToken = default);
+    Task SetDataAsync<T>(string key, T value, int timeSpan = 10, CancellationToken cancellationToken = default);
+    Task RemoveDataAsync(string key, CancellationToken cancellationToken = default);
 }
--- Infrastructure/Redis/RedisCache.cs
+++ Infrastructure/Redis/RedisCache.cs
@@ -1,8 +1,28 @@
-﻿using Microsoft.Extensions.Caching.Distributed;
+﻿using System.Text.Json;
+using Microsoft.Extensions.Caching.Distributed;
 
 namespace Infrastructure.Redis;
 
 public class RedisCache(IDistributedCache cache) : IRedisCache
 {
-    
+    public async Task<T?> GetDataAsync<T>(string key, CancellationToken cancellationToken = default)
+    {
+        var jsonData = await cache.GetStringAsync(key, cancellationToken);
+        return jsonData != null 
+            ? JsonSerializer.Deserialize<T>(jsonData)
+            : default;
+    }
+
+    public async Task SetDataAsync<T>(string key, T value, int timeSpan = 10, CancellationToken cancellationToken = default)
+    {
+        var option = new DistributedCacheEntryOptions()
+            .SetSlidingExpiration(TimeSpan.FromMinutes(timeSpan));
+        var jsonData = JsonSerializer.Serialize(value);
+        await cache.SetStringAsync(key, jsonData, option, cancellationToken);
+    }
+
+    public async Task RemoveDataAsync(string key, CancellationToken cancellationToken = default)
+    {
+        await cache.RemoveAsync(key, cancellationToken);
+    }
 }
